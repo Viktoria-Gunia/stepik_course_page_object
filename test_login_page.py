7636d9a58c827193f618a16a78c417fdb737db90
Viktoria-Gunia <viktoriagunja@gmail.com>
2022-03-01 17:37:47 +0300

    исправлено

---
+++ test_login_page.py
@@ -0,0 +1,11 @@
+import time
+from .pages.login_page import LoginPage
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
+    page = LoginPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    # page.go_to_login_page() 
+    page.should_be_login_url()        
+    # pytest -v --tb=line --language=en test_login_page.py
