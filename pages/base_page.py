48b964a083ad0d9e916042fe8c1f1bec35499895
Viktoria-Gunia <viktoriagunja@gmail.com>
2022-03-01 17:40:31 +0300

    добавлено

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,14 +1,41 @@
 from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException 
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import math
+import time
+
 class BasePage():
     def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
+      
     def open(self):
-        self.browser.get(self.url) 
+        self.browser.get(self.url)
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            WebDriverWait(self.browser, 3).until(EC.alert_is_present(),
+                                   'Timed out waiting for PA creation ' +
+                                   'confirmation popup to appear.')
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+            time.sleep(3)
+        except (NoAlertPresentException, TimeoutException):
+            print("No second alert presented") 
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
         except (NoSuchElementException):
             return False
-        return True       
+        return True
+    
