948ea3ae4a96a2aace9eaa1e4b9c599bfe48e426
Viktoria-Gunia <viktoriagunja@gmail.com>
2022-03-01 17:41:58 +0300

    добавлено

--- pages/locators.py
+++ pages/locators.py
@@ -7,3 +7,11 @@ class LoginPageLocators():
    LOGIN_URL = (By.CSS_SELECTOR, "#login_link")
    LOGIN_LOGIN_FORM = (By.CSS_SELECTOR, "#login_form") 
    LOGIN_REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
+class ProductPageLocators():
+   BTN_ADD_TO_BASKET = (By.CSS_SELECTOR, "button.btn.btn-add-to-basket") 
+   PRICE_BEFOR = (By.CSS_SELECTOR, "p.price_color")
+   IN_BASKET = (By.CSS_SELECTOR, ".alertinner strong")
+   NAME_TOVAR = (By.XPATH, "//h1")
+   
+ 
