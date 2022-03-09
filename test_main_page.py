14906bd7f56946b7f5a316a7e2b7c739b31f9f60
Viktoria-Gunia <viktoriagunja@gmail.com>
2022-03-09 21:00:53 +0300

    pytest -v --tb=line --language=en  -m smoke test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -1,11 +1,22 @@
-import time
-from .pages.login_page import LoginPage
+import pytest
+from .pages.product_page import LoginPage
+from .pages.basket_page import BasketPage
 
+@pytest.mark.smoke
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-    page = LoginPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    # page.go_to_login_page() 
+    page = LoginPage(browser, link)   
+    page.open()                     
     page.should_be_login_url()        
-    # pytest -v --tb=line --language=en test_main_page.py
+
+@pytest.mark.smoke    
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = BasketPage(browser, link)   
+    page.open()
+    page.go_to_basket_page()
+    page.should_be_empty_basket()
+    page.should_be_empty_text_basket()
+# pytest -v --tb=line --language=en  -m smoke test_main_page.py
+
+                      
