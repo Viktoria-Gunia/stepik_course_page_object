8f4cd2d359ef747910e010a800b7f58397a20e42
Viktoria-Gunia <viktoriagunja@gmail.com>
2022-03-09 20:52:28 +0300

    pytest -rx -v test_guest_can_add_product_to_basket.py

--- test_guest_can_add_product_to_basket.py
+++ test_guest_can_add_product_to_basket.py
@@ -1,8 +1,11 @@
+import pytest
 from .pages.product_page import ProductPage
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('promo_offer', ["0","1", "2","3", "4", "5", "6", "7", "8", "9"])
+def test_guest_can_add_product_to_basket(browser,promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
+    print(link)
     page = ProductPage(browser, link) 
     page.open()                     
     page.should_be_product_url()        
-    # pytest -s test_guest_can_add_product_to_basket.py
+    # pytest -rx -v test_guest_can_add_product_to_basket.py
