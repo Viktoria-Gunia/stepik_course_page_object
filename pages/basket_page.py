33c63304fa1873f72b80a8025f323179cf9853f3
Viktoria-Gunia <viktoriagunja@gmail.com>
2022-03-09 21:07:41 +0300

    проверка пустой крзины

---
+++ pages/basket_page.py
@@ -0,0 +1,13 @@
+from .base_page import BasePage
+from .locators import MainPageLocators
+
+class BasketPage(BasePage):
+    def go_to_basket_page(self):
+        link = self.browser.find_element(*MainPageLocators.GUEST_CLICK_BASKET)
+        link.click()
+
+    def should_be_empty_basket(self):
+       assert self.is_element_present(*MainPageLocators.BASKET_IS_EMPTY) == False, "Basket is not empty"
+
+    def should_be_empty_text_basket(self):
+       assert self.is_element_present(*MainPageLocators.BASKET_IS_TEXT_EMPTY) == True, "Basket is empty, but text:'Your basket is empty.' not found"      
