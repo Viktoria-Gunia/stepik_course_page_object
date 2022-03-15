4ff9b44b8bf4408d18ab6d2987285c8684d30625
Viktoria-Gunia <viktoriagunja@gmail.com>
2022-03-15 21:09:55 +0300

    authorized_user

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import math
 import time
+from .locators import BasePageLocators
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
@@ -33,7 +34,6 @@ class BasePage():
         except (NoAlertPresentException, TimeoutException):
             print("No second alert presented")
 
-    # проверяет, что элемент  появляется на странице в течение заданного времени
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
@@ -41,7 +41,6 @@ class BasePage():
             return False
         return True        
 
-    # проверяет, что элемент не появляется на странице в течение заданного времени:
     def is_not_element_present(self, how, what, timeout=4):
         try:
             WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
@@ -49,15 +48,18 @@ class BasePage():
             return True
         return False
 
-        
-    # проверить, что какой-то элемент исчезает
     def is_disappeared(self, how, what, timeout=4):
         try:
-            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
-                until_not(EC.presence_of_element_located((how, what)))
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).until_not(EC.presence_of_element_located((how, what)))
         except TimeoutException:
             return False
         return True 
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                 " probably unauthorised user" 
+
+
 
     
     
