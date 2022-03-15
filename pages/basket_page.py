e7027e736aa150c957640c209957035d0ec50e2f
Viktoria-Gunia <viktoriagunja@gmail.com>
2022-03-15 21:07:27 +0300

    go to Basket

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -1,13 +1,13 @@
 from .base_page import BasePage
-from .locators import MainPageLocators
+from .locators import BasePageLocators
 
 class BasketPage(BasePage):
     def go_to_basket_page(self):
-        link = self.browser.find_element(*MainPageLocators.GUEST_CLICK_BASKET)
+        link = self.browser.find_element(*BasePageLocators.GUEST_CLICK_BASKET)
         link.click()
 
     def should_be_empty_basket(self):
-       assert self.is_element_present(*MainPageLocators.BASKET_IS_EMPTY) == False, "Basket is not empty"
+       assert self.is_element_present(*BasePageLocators.BASKET_IS_EMPTY) == False, "Basket is not empty"
 
     def should_be_empty_text_basket(self):
-       assert self.is_element_present(*MainPageLocators.BASKET_IS_TEXT_EMPTY) == True, "Basket is empty, but text:'Your basket is empty.' not found"      
+       assert self.is_element_present(*BasePageLocators.BASKET_IS_TEXT_EMPTY) == True, "Basket is empty, but text:'Your basket is empty.' not found"      
