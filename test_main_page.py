6ab85e98e51ebc009b6c062b1f4dff53477e1302
Viktoria-Gunia <viktoriagunja@gmail.com>
2022-03-15 20:45:37 +0300

    last update

--- test_main_page.py
+++ test_main_page.py
@@ -1,22 +1,21 @@
 import pytest
-from .pages.product_page import LoginPage
-from .pages.basket_page import BasketPage
+from .pages.login_page import LoginPage
 
-@pytest.mark.smoke
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = LoginPage(browser, link)   
-    page.open()                     
-    page.should_be_login_url()        
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self,browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = LoginPage(browser, link)   
+        page.open()                     
+        page.should_be_login_url()        
 
-@pytest.mark.smoke    
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = BasketPage(browser, link)   
-    page.open()
-    page.go_to_basket_page()
-    page.should_be_empty_basket()
-    page.should_be_empty_text_basket()
-# pytest -v --tb=line --language=en  -m smoke test_main_page.py
 
-                      
+    def test_guest_should_see_login_link(self,browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = LoginPage(browser, link)
+        page.open()
+        page.should_see_login_url()  
+
+#pytest -v --tb=line --language=en  -m login_guest test_main_page.py       
+
+                    
